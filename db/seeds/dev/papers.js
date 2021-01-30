7b9351a8473afbb7bc71eb63c448bbd05a23fc37
Eduardo Parra <helloeduardo@icloud.com>
2021-01-30 16:56:23 -0700

    Add seeds

---
+++ db/seeds/dev/papers.js
@@ -0,0 +1,20 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('footnotes').del()
+    .then(() => knex('papers').del())
+    .then(() => {
+      // Inserts seed entries
+      return knex('papers').insert(
+        {title: 'Fooo', author: 'Bob', publisher: 'Minnesota'},
+      'id')
+      .then(paper => {
+        return knex('footnotes').insert([
+          { note: 'Lorem', paper_id: paper[0] },
+          { note: 'Dolor', paper_id: paper[0] }
+        ])
+      })
+      .then(() => console.log('Seeding complete!'))
+      .catch(error => console.log(`Error seeding data: ${error}`))
+    })
+  .catch(error => console.log(`Error seeding data: ${error}`));
+};
